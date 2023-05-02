b29938b148598598d56e147e55d0cc141ad1fc38
chunyi <wchunyi@foxmail.com>
2023-05-02 15:04:07 +1000

    Update file header

--- service_status.sh
+++ service_status.sh
@@ -1,8 +1,26 @@
-echo -n "Nginx: " 
-systemctl status nginx | grep Active | awk '{print $2}'
+#!/bin/bash
 
-echo -n "php-fpm: "
-systemctl status php-fpm | grep Active | awk '{print $2}'
+# *************************************************************************** #
+# *
+# * @file:restart_services.sh
+# * @author:CHUNYI WANG
+# * @date:2023-04-29 18:27
+# * @version 1.0
+# * @description: A shell script used to restart some services.
+# *
+# ************************************************************************** #
 
-echo -n "Mariadb: "
-systemctl status mariadb | grep Active | awk '{print $2}'
+service_list=(nginx php-fpm mariadb)
+
+function check_status(){
+	for (( i=0; i<${#service_list[@]}; i++ ))
+	do
+		echo -n "${service_list[$i]}: "
+		systemctl status "${service_list[$i]}" | grep 'Active' | awk '{print $2}'
+	done
+}
+
+check_status
+
+exit 0
+
