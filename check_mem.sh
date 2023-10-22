03ef32b6cbd24896f79890bc2d6f8e5cefe5b673
ChunyiWang <83103931+shitsuku-0v0@users.noreply.github.com>
2023-10-22 18:50:23 +1100

    Delete check_mem.sh

--- check_mem.sh
+++
@@ -1,6 +0,0 @@
-ava_mem=$(free -h|grep 'Mem'|awk '{print $NF}' | sed 's/[A-Z]*//g')
-email="973321662@qq.com"
-
-if test $[ava_mem] -lt 50; then 
-	echo -e "The Available memory has dropped to the warning point;\nAvailable Memory: $ava_mem " | mail -s "System Warning" "${email}"
-fi
